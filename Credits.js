35eaad93cd959825e46502b6f93342118c44d5e6
arivpns <arivpns@gmail.com>
2024-06-25 18:59:00 +0800

    cc

---
+++ Credits.js
@@ -0,0 +1,25 @@
+// Kalo mau Ambil Code Atau recode Boleh aja Tapi izin dulu kontol
+// Sell sc 34k - 57k No Rip Deck gw enc ni No jual murah Jangan lupa komen Stor kalo balik modal
+
+/* Reupload Atau Review Bot 
+ * 1. Tag me @Ziyo232
+ * 2. Tambahkan Saya ke Credits Kalo bisa
+ *\
+ 
+/* Thanks To
+> Xeon ( Base / Creator )
+> Always Ziyoo ( Saya :v / Add Fitur / Recode )
+> Always Kyuu ( KyuuDev / Add Fitur )
+> Subscriber ( Supporter )
+> Whiskeysockets ( Baileys )
+> All Creator Bot Whatsapp ( Creator / kang recode )
+> 1 orang ga di kenal ( Tester )
+> YASSxOFFC ( Teman Yg Kasih Base / Sc )
+> Jarz ( Temen / Recode Bug )
+
+[ AlwaysZiyoo From Always Compact ] */
+
+/*         Sedikit Info        */
+// Sepertinya sc Support termux Soalnya Di panel Cuman 100 mb an module nya
+// McLareN lo warna apa bos :v
+// +62
